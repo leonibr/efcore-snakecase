f5f2b20eea3de372d8a544a1bf0acf42232e777b
Brad Mastaler <brad.mastaler@computerinsights.ca>
2020-01-14 17:33:30 -0700

    Changed behavior of all-caps substring to cleave prior to last character rather than not at all.

--- tests/SnakeCaseTests/FormatTests.cs
+++ tests/SnakeCaseTests/FormatTests.cs
@@ -12,7 +12,7 @@ namespace SnakeCaseTests
         [InlineData("_Person_Log", "person_log")]
         [InlineData("Person_1Log", "person_1_log")]
         [InlineData("Person1Log", "person1_log")]
-        [InlineData("PersonLOGFile", "person_logfile")]
+        [InlineData("PersonLOGFile", "person_log_file")]
         [InlineData("PersonCodeId", "person_code_id")]
         [InlineData("PersonAddress_", "person_address_")]
         public void TestFramatNames(string given, string expcted)
