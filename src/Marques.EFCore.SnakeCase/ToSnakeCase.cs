leonibr/efcore-snakecase

commit f5f2b20eea3de372d8a544a1bf0acf42232e777b

Changed behavior of all-caps substring to cleave prior to last character rather than not at all.

src/Marques.EFCore.SnakeCase/ToSnakeCase.cs
@@ -11,7 +11,7 @@ namespace Marques.EFCore.SnakeCase
             if (string.IsNullOrEmpty(input)) { return input; }
 
             var noLeadingUndescore = Regex.Replace(input, @"^_", "");
-            return Regex.Replace(noLeadingUndescore, @"([a-z0-9])([A-Z])", "$1_$2").ToLower();
+            return Regex.Replace(noLeadingUndescore, @"(?:(?<l>[a-z0-9])(?<r>[A-Z])|(?<l>[A-Z])(?<r>[A-Z][a-z0-9]))", "${l}_${r}").ToLower();
         }
 
         public static void ToSnakeCase(this ModelBuilder modelBuilder)

tests/SnakeCaseTests/FormatTests.cs
@@ -12,7 +12,7 @@ namespace SnakeCaseTests
         [InlineData("_Person_Log", "person_log")]
         [InlineData("Person_1Log", "person_1_log")]
         [InlineData("Person1Log", "person1_log")]
-        [InlineData("PersonLOGFile", "person_logfile")]
+        [InlineData("PersonLOGFile", "person_log_file")]
         [InlineData("PersonCodeId", "person_code_id")]
         [InlineData("PersonAddress_", "person_address_")]
         public void TestFramatNames(string given, string expcted)

commit 808697b00e1e3486ada151095c48a1701d954ecd

Changed behavior of all-caps substring to cleave prior to last character rather than not at all. This corrects what I think is a regression that was merged in f5f2b20e to master on 2020-01-14.

src/Marques.EFCore.SnakeCase/ToSnakeCase.cs
@@ -18,14 +18,14 @@ namespace Marques.EFCore.SnakeCase
             Indexes = 2 << 5,
         };
         private static Regex leadingUndescoreRegex = new Regex(@"^_", RegexOptions.Compiled);
-        private static Regex camelCaseRegex = new Regex(@"([a-z0-9])([A-Z])", RegexOptions.Compiled);
+        private static Regex camelCaseRegex = new Regex(@"(?:(?<l>[a-z0-9])(?<r>[A-Z])|(?<l>[A-Z])(?<r>[A-Z][a-z0-9]))", RegexOptions.Compiled);
         
         public static string ToSnakeCase(this string input)
         {
             if (string.IsNullOrEmpty(input)) { return input; }
 
             var noLeadingUndescore = leadingUndescoreRegex.Replace(input, "");
-            return camelCaseRegex.Replace(noLeadingUndescore, "$1_$2").ToLower();
+            return camelCaseRegex.Replace(noLeadingUndescore, "${l}_${r}").ToLower();
         }
 
         public static void ToSnakeCase(this ModelBuilder modelBuilder, ConvertOptions options = ConvertOptions.All)

tests/SnakeCaseTests/FormatTests.cs
@@ -12,10 +12,10 @@ namespace SnakeCaseTests
         [InlineData("_Person_Log", "person_log")]
         [InlineData("Person_1Log", "person_1_log")]
         [InlineData("Person1Log", "person1_log")]
-        [InlineData("PersonLOGFile", "person_logfile")]
+        [InlineData("PersonLOGFile", "person_log_file")]
         [InlineData("PersonCodeId", "person_code_id")]
         [InlineData("PersonAddress_", "person_address_")]
-        public void TestFramatNames(string given, string expcted)
+        public void TestFormatNames(string given, string expcted)
         {
             given.ToSnakeCase().ShouldBe(expcted);
         }